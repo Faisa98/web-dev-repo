7a57a122c2733f6aa4e0c79c0678c2019475cbc6
Faisal98 <faisal1alsayyaf@hotmail.com>
2025-10-06 11:39:11 +0300

    copy from lab5

---
+++ lab6/libraryproject/apps/bookmodule/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# def index(request):
+#     name = request.GET.get("name") or "world!"
+#     return render(request, "bookmodule/index.html", {"name": name})
+
+def index2(request, val1 = 0):
+    return render(request, "bookmodule/index2.html", {"val1": val1})
+
+def viewBook(request, bookId):
+    # assume that we have the following books somewhere (e.g. database)
+    book1 = {'id':123, 'title':'Continuous Delivery', 'author':'J. Humble and D. Farley'}
+    book2 = {'id':456, 'title':'Secrets of Reverse Engineering', 'author':'E. Eilam'}
+    targetBook = None
+    if book1['id'] == bookId: targetBook = book1
+    if book2['id'] == bookId: targetBook = book2
+    context = {'book':targetBook} # book is the variable name accessible by the template
+    return render(request, 'bookmodule/show.html', context)
+
+def index(request):
+    return render(request, "bookmodule/index.html")
+def list_books(request):
+    return render(request, 'bookmodule/list_books.html')
+def viewbook(request, bookId):
+    return render(request, 'bookmodule/one_book.html')
+def aboutus(request):
+    return render(request, 'bookmodule/aboutus.html')
+
+def links(request):
+    return render(request, 'bookmodule/links.html')
+def text(request):
+    return render(request, 'bookmodule/text.html')
+def listing(request):
+    return render(request, 'bookmodule/listing.html')
+def tables(request):
+    return render(request, 'bookmodule/tables.html')
