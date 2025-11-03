fbae511ffd604323be78d34129df17ec6911dc37
Faisal98 <faisal1alsayyaf@hotmail.com>
2025-11-03 10:22:57 +0300

    Initialize Django project structure for library management system
    - Created __init__.py for libraryproject package.
    - Added ASGI configuration for asynchronous server gateway interface.
    - Set up settings.py with essential configurations including installed apps, middleware, database settings, and static files.
    - Configured URL routing for admin and app-specific URLs in urls.py.
    - Implemented WSGI configuration for web server gateway interface.
    - Added manage.py for command-line administrative tasks.

---
+++ lab8/libraryproject/apps/bookmodule/urls.py
@@ -0,0 +1,30 @@
+from django.urls import include, path
+from . import views
+
+urlpatterns = [
+    # path('', views.index),
+    path('index2/<int:val1>/', views.index2),
+    # path('<int:bookId>', views.viewBook),
+    
+    path('', views.index, name= "books.index"),
+    path('list_books/', views.list_books, name= "books.list_books"),
+    path('<int:bookId>/', views.viewbook, name="books.view_one_book"),
+    path('aboutus/', views.aboutus, name="books.aboutus"),
+
+    path('html5/', include('apps.bookmodule.html5_urls')),
+    
+    #lab6
+    path('search/', views.search, name="books.search"),
+
+    #lab7
+    path('simple/query/', views.simple_query, name="books.simple_query"),
+    path('complex/query/', views.complex_query, name="books.complex_query"),
+    
+    #lab8
+    path('lab8/task1/', views.task1, name="books.task1"),
+    path('lab8/task2/', views.task2, name="books.task2"),
+    path('lab8/task3/', views.task3, name="books.task3"),
+    path('lab8/task4/', views.task4, name="books.task4"),
+    path('lab8/task5/', views.task5, name="books.task5"),
+
+]
