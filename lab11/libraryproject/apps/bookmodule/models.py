a4ba0d81d0910f516679bc602689cd3c5c93a901
Faisa98 <faisal1alsayyaf@hotmail.com>
2025-12-07 09:15:07 +0300

    copy from lab 8

---
+++ lab11/libraryproject/apps/bookmodule/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length = 50)
+    author = models.CharField(max_length = 50)
+    price = models.FloatField(default = 0.0)
+    edition = models.SmallIntegerField(default = 1)
+    
+class Address(models.Model):
+    city= models.CharField(max_length=100)
+
+class Student(models.Model):
+    name= models.CharField(max_length=50)
+    age= models.SmallIntegerField(default=0)
+    address = models.ForeignKey(Address, on_delete=models.PROTECT,null=True)
+    
